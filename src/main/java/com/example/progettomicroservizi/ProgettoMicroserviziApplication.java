14754c97ed9fa149aa45cd532e832e99a137a794
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-17 11:10:13 +0200

    first commit microservices project

---
+++ src/main/java/com/example/progettomicroservizi/ProgettoMicroserviziApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettomicroservizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoMicroserviziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoMicroserviziApplication.class, args);
+    }
+
+}
