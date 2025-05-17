090b55e87a6b66ba695d5e3154c1adf348aebb06
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-17 15:44:41 +0200

    basic bank-account project gg

--- src/main/java/com/example/progettomicroservizi/ProgettoMicroserviziApplication.java
+++ src/main/java/com/example/progettomicroservizi/ProgettoMicroserviziApplication.java
@@ -2,8 +2,10 @@ package com.example.progettomicroservizi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class ProgettoMicroserviziApplication {
 
     public static void main(String[] args) {
